286f4900d8c504002cc8a96aa02a23ddaf6ab9be
Undefined01 <amoscr@163.com>
2021-03-02 10:33:38 +0800

    Ray and shpere

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
 typedef struct {
@@ -27,13 +29,33 @@ float vec3_len(const vec3 v) { return sqrt(vec3_dot(v, v)); }
 vec3 vec3_unit(const vec3 v) { return vec3_sdiv(v, vec3_len(v)); }
 
 typedef struct {
+    // 起始位置和方向
     vec3 pos, ori;
 } ray;
 
+typedef struct {
+    vec3 pos;
+    float r;
+} sphere;
+bool hit_sphere(const ray *r, const sphere *s) {
+    vec3 oc = vec3_sub(r->pos, s->pos);
+    float a = vec3_dot(r->ori, r->ori);
+    float b = vec3_dot(oc, r->ori);
+    float c = vec3_dot(oc, oc) - s->r * s->r;
+    return b * b - a * c > 0;
+}
+
 vec3 color(ray *r) {
+    // (0, 0, -1) 处的红色球
+    // 此时 hit_sphere 只能判断光所在直线与球是否相交。
+    // 因此把球的 z 坐标设为 +1 后仍然能看到。
+    // 后续将会继续改进。
+    sphere s = {{{0.0, 0.0, -1.0}}, 0.5};
+    if (hit_sphere(r, &s))
+        return (vec3){{1.0, 0.0, 0.0}};
+    // 生成上蓝下白的渐变作为背景
     vec3 ori = vec3_unit(r->ori);
     float t = 0.5 * ori.d[1];
-    // 生成上蓝下白的渐变
     return vec3_add(vec3_smul((vec3){{1.0, 1.0, 1.0}}, 0.5 - t),
                     vec3_smul((vec3){{0.5, 0.7, 1.0}}, 0.5 + t));
 }
@@ -49,6 +71,9 @@ int main() {
             float u = (float)i / (float)nx - 0.5;
             float v = (float)j / (float)ny - 0.5;
 
+            // 光栅化，将从原点到
+            // (-2, -1, -1) -- (2, -1, -1) -- (2, 1, -1) -- (-2, 1, -1)
+            // 这一锥形区域内的图像投影到 200x100 的画布上
             ray r = {origin, {{u * 4, v * 2, -1.0}}};
             vec3 col = color(&r);
 
